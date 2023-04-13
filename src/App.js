5f4c15c29453a6fc4fc3d0723748692dbd090fce
megan-boyd <megan.boyd8@gmail.com>
2023-04-12 21:33:24 -0500

    msg

--- src/App.js
+++ src/App.js
@@ -669,7 +669,7 @@ if (username.value.length === 0) {
         </div>
         <div className='text-center'>
           <button type='button' className='btn btn-danger m-2' onClick={e => { setCart([]); changePageView("Browse"); }}>Cancel</button>
-          <button type='button' className='btn btn-danger m-2' onClick={e => { alert("Thank you for your order!"); setCart([]); changePageView("browse"); }}>Confirm</button>
+          <button type='button' className='btn btn-danger m-2' onClick={e => { alert("Thank you come again"); setCart([]); changePageView("browse"); }}>Confirm</button>
         </div>
       </div>
     );
